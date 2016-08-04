431ab276b3b0648caf451112af78812b71593ca8
Jason Lenoble <jason.lenoble@gmail.com>
2016-08-04 15:39:25 +0200

    Dependency glob changed to glob-all
    Tests for plugin with glob arg or glob list

--- src/gulp-error.js
+++ src/gulp-error.js
@@ -1,26 +1,54 @@
 import {PluginError} from 'gulp-util';
 import through from 'through2';
 import path from 'path';
+import globAll from 'glob-all';
 
 const PLUGIN_NAME = 'gulp-error';
 
 export default function(files = []) {
-  if (!Array.isArray(files)) {
+  if (files === undefined) {
+    files = [];
+  } else if (typeof files === 'string') {
     files = [files];
   }
 
-  return through.obj(function(file, encoding, callback) {
-    const relative =  path.relative(process.cwd(), file.path);
+  if (!Array.isArray(files)) {
+    throw new PluginError(PLUGIN_NAME, `Unexpected options: ${files}`);
+  }
 
-    if (files.length === 0 || files.includes(relative)) {
+  var promise = files.length ? new Promise((resolve, reject) => {
+    globAll(files, function(err, _files) {
+      if (err) {
+        reject(err);
+      }
 
-      this.emit('error', new PluginError(PLUGIN_NAME,
-        `Intentional error when processing file ${relative}`));
+      resolve(_files);
+    });
+  }) : Promise.resolve(files);
 
-    } else {
+  return (function(promise) {
 
-      callback(null, file);
+    return through.obj(function(file, encoding, callback) {
+      const relative =  path.relative(process.cwd(), file.path);
 
-    }
-  });
+      promise.then(_files => {
+        if (_files.length === 0 || _files.includes(relative)) {
+
+          this.emit('error', new PluginError(PLUGIN_NAME,
+            `Intentional error when processing file ${relative}`));
+
+        } else {
+
+          callback(null, file);
+
+        }
+
+      }).catch(err => {
+        new PluginError(PLUGIN_NAME, err);
+      });
+
+    });
+
+  })(promise);
+
 }
--- test/gulp-error.test.js
+++ test/gulp-error.test.js
@@ -33,10 +33,67 @@ describe('Test suite for Gulp plugin gulp-error:', () => {
     });
   });
 
+  it('gulp-error with file arg throws on that file', function() {
+    return new Promise((resolve, reject) => {
+      gulp.src(glob).pipe(error('src/gulp-error.js'))
+        .on('error', () => {
+          const logs = this.error.getLogs();
+
+          expect(logs).to.match(
+            /.*Error.*Intentional error when processing.*src\/gulp-error\.js.*/);
+          expect(console.error.callCount).to.equal(1);
+
+          resolve();
+        })
+        .on('finish', () => {
+          reject(new Error(
+            'No error was thrown, but that should have happened'));
+        });
+    });
+  });
+
   it('gulp-error with filelist arg throws on first file matched', function() {
     return new Promise((resolve, reject) => {
       gulp.src(glob).pipe(error(['test/gulp-error.test.js',
         'src/gulp-error.js']))
+        .on('error', () => {
+          const logs = this.error.getLogs();
+
+          expect(logs).to.match(
+            /.*Error.*Intentional error when processing.*src\/gulp-error\.js.*/);
+          expect(console.error.callCount).to.equal(1);
+
+          resolve();
+        })
+        .on('finish', () => {
+          reject(new Error(
+            'No error was thrown, but that should have happened'));
+        });
+    });
+  });
+
+  it('gulp-error with glob arg throws on first file matched', function() {
+    return new Promise((resolve, reject) => {
+      gulp.src(glob).pipe(error('src/*.js'))
+        .on('error', () => {
+          const logs = this.error.getLogs();
+
+          expect(logs).to.match(
+            /.*Error.*Intentional error when processing.*src\/gulp-error\.js.*/);
+          expect(console.error.callCount).to.equal(1);
+
+          resolve();
+        })
+        .on('finish', () => {
+          reject(new Error(
+            'No error was thrown, but that should have happened'));
+        });
+    });
+  });
+
+  it('gulp-error with globlist arg throws on first file matched', function() {
+    return new Promise((resolve, reject) => {
+      gulp.src(glob).pipe(error(['test/**/*.js', 'src/**/*.js']))
         .on('error', () => {
           const logs = this.error.getLogs();
 
