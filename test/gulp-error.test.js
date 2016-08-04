ac4f4225ec17d154ca1294ff5fabea83c0511fe1
Jason Lenoble <jason.lenoble@gmail.com>
2016-08-04 20:23:28 +0200

    Factorized test callbacks with a factory

--- test/gulp-error.test.js
+++ test/gulp-error.test.js
@@ -14,101 +14,51 @@ describe('Test suite for Gulp plugin gulp-error:', () => {
     this.error.mute();
   });
 
-  it('gulp-error with no arg throws on first file passed', function() {
-    return new Promise((resolve, reject) => {
-      gulp.src(glob).pipe(error())
-        .on('error', () => {
-          const logs = this.error.getLogs();
+  const testArgCallback = function(options) {
+    return function() {
+      return new Promise((resolve, reject) => {
+        gulp.src(glob).pipe(error(options.arg))
+          .on('error', () => {
+            const logs = this.error.getLogs();
 
-          expect(logs).to.match(
-            /.*Error.*Intentional error when processing.*gulpfile\.babel\.js.*/);
-          expect(console.error.callCount).to.equal(1);
+            expect(logs).to.match(options.match);
+            expect(console.error.callCount).to.equal(1);
 
-          resolve();
-        })
-        .on('finish', () => {
-          reject(new Error(
-            'No error was thrown, but that should have happened'));
-        });
-    });
-  });
+            resolve();
+          })
+          .on('finish', () => {
+            reject(new Error(
+              'No error was thrown, but that should have happened'));
+          });
+      });
+    };
+  };
 
-  it('gulp-error with file arg throws on that file', function() {
-    return new Promise((resolve, reject) => {
-      gulp.src(glob).pipe(error('src/gulp-error.js'))
-        .on('error', () => {
-          const logs = this.error.getLogs();
+  it('gulp-error with no arg throws on first file passed', testArgCallback({
+    match: /.*Error.*Intentional error when processing.*gulpfile\.babel\.js.*/
+  }));
 
-          expect(logs).to.match(
-            /.*Error.*Intentional error when processing.*src\/gulp-error\.js.*/);
-          expect(console.error.callCount).to.equal(1);
+  it('gulp-error with file arg throws on that file', testArgCallback({
+    arg: 'test/gulp-error.test.js',
+    match: /.*Error.*Intentional error when processing.*test\/gulp-error\.test\.js.*/
+  }));
 
-          resolve();
-        })
-        .on('finish', () => {
-          reject(new Error(
-            'No error was thrown, but that should have happened'));
-        });
-    });
-  });
+  it('gulp-error with filelist arg throws on first file matched',
+  testArgCallback({
+    arg: ['test/gulp-error.test.js', 'src/gulp-error.js'],
+    match: /.*Error.*Intentional error when processing.*src\/gulp-error\.js.*/
+  }));
 
-  it('gulp-error with filelist arg throws on first file matched', function() {
-    return new Promise((resolve, reject) => {
-      gulp.src(glob).pipe(error(['test/gulp-error.test.js',
-        'src/gulp-error.js']))
-        .on('error', () => {
-          const logs = this.error.getLogs();
+  it('gulp-error with glob arg throws on first file matched', testArgCallback({
+    arg: 'src/*.js',
+    match: /.*Error.*Intentional error when processing.*src\/gulp-error\.js.*/
+  }));
 
-          expect(logs).to.match(
-            /.*Error.*Intentional error when processing.*src\/gulp-error\.js.*/);
-          expect(console.error.callCount).to.equal(1);
-
-          resolve();
-        })
-        .on('finish', () => {
-          reject(new Error(
-            'No error was thrown, but that should have happened'));
-        });
-    });
-  });
-
-  it('gulp-error with glob arg throws on first file matched', function() {
-    return new Promise((resolve, reject) => {
-      gulp.src(glob).pipe(error('src/*.js'))
-        .on('error', () => {
-          const logs = this.error.getLogs();
-
-          expect(logs).to.match(
-            /.*Error.*Intentional error when processing.*src\/gulp-error\.js.*/);
-          expect(console.error.callCount).to.equal(1);
-
-          resolve();
-        })
-        .on('finish', () => {
-          reject(new Error(
-            'No error was thrown, but that should have happened'));
-        });
-    });
-  });
-
-  it('gulp-error with globlist arg throws on first file matched', function() {
-    return new Promise((resolve, reject) => {
-      gulp.src(glob).pipe(error(['test/**/*.js', 'src/**/*.js']))
-        .on('error', () => {
-          const logs = this.error.getLogs();
-
-          expect(logs).to.match(
-            /.*Error.*Intentional error when processing.*gulp-error\.js.*/);
-          expect(console.error.callCount).to.equal(1);
-
-          resolve();
-        })
-        .on('finish', () => {
-          reject(new Error(
-            'No error was thrown, but that should have happened'));
-        });
-    });
-  });
+  it('gulp-error with globlist arg throws on first file matched',
+  testArgCallback({
+    arg: ['test/**/*.js', 'src/**/*.js'],
+    match: /.*Error.*Intentional error when processing.*src\/gulp-error\.js.*/
+  }));
 
   afterEach(function() {
     this.error.unmute();
